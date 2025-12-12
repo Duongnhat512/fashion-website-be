21b4a4aa97f725c1ccdf7d2d5a26a84fff1cf6b5
duongnhat512 <duongnhat512@gmail.com>
2025-12-12 14:10:34 +0700

    refactor: enhance category routes with admin restrictions and file upload
    - Added middleware for admin-only access to category creation, update, and deletion routes.
    - Integrated file upload handling for category creation and update processes, improving functionality and security.
    - Streamlined route definitions for better clarity and maintainability.

--- app/routers/category.route.ts
+++ app/routers/category.route.ts
@@ -1,14 +1,16 @@
 import { CategoryController } from '../controllers/category.controller';
 import { Router } from 'express';
+import { uploadSingle } from '../middlewares/upload.middleware';
+import { adminOnly } from '../middlewares/auth.middleware';
 
 const categoryController = new CategoryController();
 
 const router = Router();
 
 router.get('/tree', (req, res) => categoryController.getCategoryTree(req, res));
-router.post('/', (req, res) => categoryController.createCategory(req, res));
-router.put('/', (req, res) => categoryController.updateCategory(req, res));
-router.post('/delete/:id', (req, res) =>
+router.post('/', adminOnly, uploadSingle, (req, res) => categoryController.createCategory(req, res));
+router.put('/', adminOnly, uploadSingle, (req, res) => categoryController.updateCategory(req, res));
+router.post('/delete/:id', adminOnly, (req, res) =>
   categoryController.deleteCategory(req, res),
 );
 router.get('/get-by-id', (req, res) =>
